5cb43df6dc20887e84417cd4cdbc652d6d2b5cd6
Faiqa-Iqrar <faiqa.iqrarr@gmail.com>
2025-02-04 23:55:36 +0500

    Added For Loops

---
+++ .dart_tool/dartbasics.dart/WhileLoops.dart
@@ -0,0 +1,21 @@
+void main() {
+//   // For IN LOOP:
+
+  String value = 'Hello';
+  for (int i = 0; i < value.length; i++) {
+    print(value[i]);
+  }
+
+//   // WHILE LOOP:
+//   // In while loop you have a condition and it'll only run till the condition is true
+
+  int i = 0;
+
+  while (i < value.length) {
+    print(value[4]);
+    i++;
+  }
+
+//   String d = '*';
+//     print(d*7);
+}
